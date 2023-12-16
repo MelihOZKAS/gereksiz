b58eeeb22ba73caa09cb09b46d9f8994ac6405a7
MelihOZKAS <melihozkass@gmail.com>
2023-12-17 01:17:11 +0300

    ALLAH ım utandırma

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('story-preview/<slug:slug>/', views.StoryPreviewView.as_view(), name='story_preview'),
 
     path("add-post/", views.post_add),
+    path("real-add-post/", views.real_post_add),
 
     path("mahsul-yakala/", views.mahsulyakala),
     path("mahsul-listesi-cek/", views.mahsullistesicek),
