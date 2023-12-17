5fe9dc94ee426f60616697c5be1e8c2e3a71e82f
MelihOZKAS <melihozkass@gmail.com>
2023-12-17 03:51:29 +0300

    ALLAH ım utandırma

--- blog/views.py
+++ blog/views.py
@@ -352,10 +352,10 @@ def real_post_add(request):
         Post_Turu_Gelen = PostKategori.objects.get(short_title=Post_Turu)
 
         title, slug = create_unique_title_slug(title)
-        siir_masal = Post(title=title, hiddenTitle=hiddenTitle,  slug=slug, h1=h1, hiddenH1=hiddenH1, Post_Turu=Post_Turu_Gelen, icerik=icerik, info=info, hiddenKeys=hiddenKeys, meta_description=meta_description, Akibeti="Beklemede", Kaynak_Linki=Kaynak_Linki)
+        siir_masal = Post(title=title, hiddenTitle=hiddenTitle,  slug=slug, h1=h1, hiddenH1=hiddenH1, Post_Turu=Post_Turu_Gelen, icerik=icerik, info=info, hiddenKeys=hiddenKeys, meta_description=meta_description, Kaynak_Linki=Kaynak_Linki)
         siir_masal.save()
 
-        #Kontrol.objects.filter(id=GelenID).update(Akibeti="Tamamlandi")
+        Kontrol.objects.filter(id=GelenID).update(Akibeti="Tamamlandi")
 
         if siir_masal.id is None:
             return HttpResponse("Post kaydedilemedi.")
