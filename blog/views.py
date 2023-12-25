00e8b250b91b3c0be56b0c2423861f92e3bd9fb9
MelihOZKAS <melihozkass@gmail.com>
2023-12-25 21:18:09 +0300

    ALLAH ım utandırma

--- blog/views.py
+++ blog/views.py
@@ -518,6 +518,8 @@ def karepostcek(request):
 
 def send_Telegrampost(request):
     GelenPost = Post.objects.filter(aktif=True, status="Yayinda",gonder=True).order_by('-olusturma_tarihi').first()
+    GelenPost.gonder=False
+    GelenPost.save()
     if GelenPost:
         title = GelenPost.title  # Postun başlığını al
         slug = GelenPost.slug  # Postun slug'ını al
