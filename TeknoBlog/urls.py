981d2837e3fc4d8b11c12ee8a09e54991871ff85
MelihOZKAS <melihozkass@gmail.com>
2024-03-16 21:23:09 +0300

    ALLAH ım utandırma

--- TeknoBlog/urls.py
+++ TeknoBlog/urls.py
@@ -26,12 +26,13 @@ urlpatterns = [
     path('sitemap-<section>.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
     path("robots.txt", robots_txt, name="robots"),
+    path('feeds/', MainHaberleriFeed(), name='feeds'),
     path('feeds/teknoloji/', TeknolojiHaberleriFeed(), name='teknoloji_feed'),
     path('feeds/bilim/', BilimHaberleriFeed(), name='bilim_feed'),
     path('feeds/otomobil/', OtomobilHaberleriFeed(), name='otomobil_feed'),
     path('feeds/oyun/', OyunHaberleriFeed(), name='oyun_feed'),
     path('feeds/telefon/', TelefonHaberleriFeed(), name='telefon_feed'),
-    path('feeds/', MainHaberleriFeed(), name='feeds'),
+
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                            document_root=settings.MEDIA_ROOT)
