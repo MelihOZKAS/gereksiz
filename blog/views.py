ea0f84311d6f5b70c2e88d991904430137fa92b6
MelihOZKAS <melihozkass@gmail.com>
2024-03-29 01:27:41 +0300

    ALLAH ım utandırma

--- blog/views.py
+++ blog/views.py
@@ -430,7 +430,7 @@ def real_post_add(request):
 
         Post_Turu_Gelen = PostKategori.objects.get(short_title=Post_Turu)
 
-        NewSlug = f"{Post_Turu}-haberleri"
+        NewSlug = f"{request.POST.get('Post_Turu')}-haberleri"
         slug = create_unique_title_slug(NewSlug)
         siir_masal = Post(ozet=ZekaOzet, title=title, hiddenTitle=hiddenTitle,  slug=slug, h1=h1, hiddenH1=hiddenH1, Post_Turu=Post_Turu_Gelen, icerik=icerik, info=info, hiddenKeys=hiddenKeys, meta_description=meta_description, Kaynak_Linki=Kaynak_Linki)
         siir_masal.save()
