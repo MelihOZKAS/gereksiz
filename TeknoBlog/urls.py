db80e105d6381f3e2a59f0ac8e938d8d36a45783
MelihOZKAS <melihozkass@gmail.com>
2024-07-24 11:16:44 +0300

    ALLAH ım utandırma

--- TeknoBlog/urls.py
+++ TeknoBlog/urls.py
@@ -16,7 +16,7 @@ sitemaps = {
 }
 
 def handler404(request, *args, **argv):
-    response = render(request, '404.html')
+    response = render(request, 'yeni-404.html')
     response.status_code = 404
     return response
 
